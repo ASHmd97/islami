7c749a5705f8f621f3c2ad788d756c5d5d9d2761
Ahmed Safwat <ahmed.hmd1996@gmail.com>
2024-10-17 08:15:37 +0300

    finish qur2an tab

--- lib/tabs/quran/sura_content_screen.dart
+++ lib/tabs/quran/sura_content_screen.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islami/tabs/quran/quran_tab.dart';
+import 'package:islami/widgets/loading_indicator.dart';
 
 // ignore: must_be_immutable
-class SuraContentScreen extends StatelessWidget {
+class SuraContentScreen extends StatefulWidget {
   SuraContentScreen({super.key});
   static const String routeName = '/sura_content';
+
+  @override
+  State<SuraContentScreen> createState() => _SuraContentScreenState();
+}
+
+class _SuraContentScreenState extends State<SuraContentScreen> {
+  List<String> suraContent = [];
   late SuraDataArgs args;
-  static const List<String> suraContent = [
-    'بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم',
-    'الحمد لله رب العالمين',
-    'الرَّحْمَنِ الرَّحِيمِ',
-    'مَالِكِ يَوْمِ الدِّينِ',
-    'إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ',
-    'اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ',
-    'صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّالِّين'
-  ];
 
   @override
   Widget build(BuildContext context) {
     args = ModalRoute.of(context)!.settings.arguments as SuraDataArgs;
 
+    if (suraContent.isEmpty) {
+      loudSuraFile();
+    }
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
@@ -29,25 +32,40 @@ class SuraContentScreen extends StatelessWidget {
       ),
       child: Scaffold(
         appBar: AppBar(title: Text(args.suraName)),
-        body: Container(
-          padding: const EdgeInsets.all(20.0),
-          margin: EdgeInsets.symmetric(
-              horizontal: 26,
-              vertical: MediaQuery.sizeOf(context).height * 0.05),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(25.0),
-          ),
-          child: ListView.builder(
-            itemCount: suraContent.length,
-            itemBuilder: (context, index) {
-              return Text(suraContent[index],
-                  style: Theme.of(context).textTheme.headlineSmall,
-                  textAlign: TextAlign.center);
-            },
-          ),
-        ),
+        body: suraContent.isEmpty
+            ? const LoadingIndicator()
+            : Container(
+                padding: const EdgeInsets.all(20.0),
+                margin: EdgeInsets.symmetric(
+                    horizontal: 26,
+                    vertical: MediaQuery.sizeOf(context).height * 0.05),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                child: ListView.separated(
+                  itemCount: suraContent.length,
+                  itemBuilder: (context, index) {
+                    return Text(suraContent[index].trim(),
+                        style: Theme.of(context).textTheme.headlineSmall,
+                        textAlign: TextAlign.center);
+                  },
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(
+                      height: 12,
+                    );
+                  },
+                ),
+              ),
       ),
     );
+  }
+
+  Future<void> loudSuraFile() async {
+    await Future.delayed(const Duration(seconds: 1));
+    String sura =
+        await rootBundle.loadString('assets/text/${args.suraIndex + 1}.txt');
+    suraContent = sura.trim().split('\n');
+    setState(() {});
   }
 }
