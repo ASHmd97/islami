953cdd6f6d701254efd73c4d0cbe4bf174384f81
ASHmd97 <ahmed.hmd1996@gmail.com>
2025-01-07 03:40:57 +0200

    add verse marker on sura

--- lib/tabs/quran/sura_content_screen.dart
+++ lib/tabs/quran/sura_content_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:islami/app_theme.dart';
 import 'package:islami/tabs/quran/quran_tab.dart';
+import 'package:islami/tabs/quran/verse_marker.dart';
 import 'package:islami/widgets/loading_indicator.dart';
 
 // ignore: must_be_immutable
 class SuraContentScreen extends StatefulWidget {
-  SuraContentScreen({super.key});
+  const SuraContentScreen({super.key});
   static const String routeName = '/sura_content';
 
   @override
@@ -24,48 +26,61 @@ class _SuraContentScreenState extends State<SuraContentScreen> {
       loudSuraFile();
     }
     return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/default_bg.png'),
-          fit: BoxFit.fill,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/default_bg.png'),
+            fit: BoxFit.fill,
+          ),
         ),
-      ),
-      child: Scaffold(
-        appBar: AppBar(title: Text(args.suraName)),
-        body: suraContent.isEmpty
-            ? const LoadingIndicator()
-            : Container(
-                padding: const EdgeInsets.all(20.0),
-                margin: EdgeInsets.symmetric(
-                    horizontal: 26,
-                    vertical: MediaQuery.sizeOf(context).height * 0.05),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-                child: ListView.separated(
-                  itemCount: suraContent.length,
-                  itemBuilder: (context, index) {
-                    return Text(suraContent[index].trim(),
-                        style: Theme.of(context).textTheme.headlineSmall,
-                        textAlign: TextAlign.center);
-                  },
-                  separatorBuilder: (context, index) {
-                    return const SizedBox(
-                      height: 12,
-                    );
-                  },
-                ),
-              ),
-      ),
-    );
+        child: Scaffold(
+            appBar: AppBar(title: Text(args.suraName)),
+            body: suraContent.isEmpty
+                ? const LoadingIndicator()
+                : Container(
+                    padding: const EdgeInsets.all(20.0),
+                    margin: EdgeInsets.symmetric(
+                        horizontal: 26,
+                        vertical: MediaQuery.sizeOf(context).height * 0.05),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    child: ListView.builder(
+                      itemCount: suraContent.length,
+                      itemBuilder: (context, index) {
+                        return RichText(
+                          textAlign: TextAlign.center,
+                          textDirection: TextDirection.rtl,
+                          text: TextSpan(
+                            style: Theme.of(context).textTheme.headlineSmall,
+                            children: [
+                              TextSpan(
+                                text: suraContent[index].trim(),
+                              ),
+                              WidgetSpan(
+                                alignment: PlaceholderAlignment.middle,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(right: 12.0),
+                                  child: VerseMarker(verseNumber: index + 1),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  )));
   }
 
   Future<void> loudSuraFile() async {
     await Future.delayed(const Duration(seconds: 1));
     String sura =
         await rootBundle.loadString('assets/text/${args.suraIndex + 1}.txt');
-    suraContent = sura.trim().split('\n');
+    suraContent = sura
+        .trim()
+        .split('\n')
+        .where((line) => line.trim().isNotEmpty)
+        .toList();
     setState(() {});
   }
 }
